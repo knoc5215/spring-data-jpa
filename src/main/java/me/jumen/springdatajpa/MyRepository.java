817363027c0f4588eb0798229a31e612cd7c4639
knoc5215 <knoc5215@naver.com>
2021-01-11 21:05:45 +0900

    스프링 데이터 Common 3. Null 처리

--- src/main/java/me/jumen/springdatajpa/MyRepository.java
+++ src/main/java/me/jumen/springdatajpa/MyRepository.java
@@ -2,19 +2,25 @@ package me.jumen.springdatajpa;
 
 import org.springframework.data.repository.NoRepositoryBean;
 import org.springframework.data.repository.Repository;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Custom Repositry Define.
- * */
+ */
 @NoRepositoryBean
 public interface MyRepository<T, Id extends Serializable> extends Repository<T, Id> {
 
-    <E extends T> E save(E entity);
+    <E extends T> E save(@NonNull E entity);
 
     List<T> findAll();
 
     long count();
+
+    @Nullable
+    <E extends T> Optional<E> findById(@Nullable Id id);
 }
--- src/test/java/me/jumen/springdatajpa/CommentRepositoryTest.java
+++ src/test/java/me/jumen/springdatajpa/CommentRepositoryTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -23,13 +24,23 @@ class CommentRepositoryTest {
         commentRepository.save(comment);
 
         //Then
-        List<Comment> all = commentRepository.findAll();
+        List<Comment> all = commentRepository.findAll();    // Collection은 null이 아닌, empty인 것을 리턴한다.
         assertThat(all.size()).isEqualTo(1);
 
         //When
         long count = commentRepository.count();
         //Then
         assertThat(count).isEqualTo(1);
+
+
+    }
+
+    @Test
+    public void optionalTest() {
+        //When
+        Optional<Comment> byId = commentRepository.findById(100l);
+        assertThat(byId).isEmpty(); // optional은 null을 리턴하지 않기에 empty를 체크해야 함.
+
 
     }
 
